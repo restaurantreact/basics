b456f011aacde4a03bdd8c9fa502c0c56282af7a
rajeshraj1998 <srsrajesh5@gmail.com>
2020-07-10 17:28:10 +0530

    jwefhdg

---
+++ App.js
@@ -0,0 +1,16 @@
+import React,{Component} from 'react';
+import {View, Text, StyleSheet, Image} from 'react-native';
+
+  const App = () => {
+  return(
+      <View>
+        <Text >
+           Hello world   
+           version --- 1
+           developer 1 branch
+         </Text>  
+        
+      </View>
+    );
+  }
+export default App;
